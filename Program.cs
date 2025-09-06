fdb9a5ee481fd98b4eb1204d2050ec67ea72a7f0
Malcolm Tyler <malcolm@malcsdomain.co.uk>
2025-09-06 13:52:49 +0100

    Update Swagger configuration to be available in both Development and Production environments

--- Program.cs
+++ Program.cs
@@ -29,9 +29,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
